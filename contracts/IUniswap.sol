224cc152f57213078422cd5681231ea72ac8dbfc
Ayoade <abdulrahmanadesile@yahoo.com>
2023-09-15 00:58:53 +0100

    initia commit

--- contracts/IUniswap.sol
+++ contracts/IUniswap.sol
@@ -56,10 +56,17 @@ interface IUniswapV2Router01 {
 }
 
 interface IUniswapV2Factory {
-    function getPair(address tokenA, address tokenB) external view returns (address pair);
-}
+    event PairCreated(address indexed token0, address indexed token1, address pair, uint);
 
-interface IUniswapV2Pair {
-    function balanceOf(address owner) external view returns (uint);
-    function approve(address spender, uint value) external returns (bool);
+    function feeTo() external view returns (address);
+    function feeToSetter() external view returns (address);
+
+    function getPair(address tokenA, address tokenB) external view returns (address pair);
+    function allPairs(uint) external view returns (address pair);
+    function allPairsLength() external view returns (uint);
+
+    function createPair(address tokenA, address tokenB) external returns (address pair);
+
+    function setFeeTo(address) external;
+    function setFeeToSetter(address) external;
 }
--- scripts/uniswap2.ts
+++ scripts/uniswap2.ts
@@ -2,31 +2,45 @@ import { ethers } from "hardhat";
 
 async function main() { 
        //uniswap router address
-  /*const ROUTER = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
+  const ROUTER = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
   // UNISWAP V2 FACTORY ADDRESS
   const FACTORY = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
 
-    const DAI = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
-    //uni token address
     const UNI = "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984";
+
+    //uni token address
+
+    const DAI = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+    //dai token address
+    
+    const currentTimestampInSeconds = Math.round(Date.now() / 1000)
+  const deadline = currentTimestampInSeconds + 86400
 
       //dai holder
   //const DAIHolder = "0x748dE14197922c4Ae258c7939C7739f3ff1db573";
   const DAIHolder = "0x20bB82F2Db6FF52b42c60cE79cDE4C7094Ce133F"
 
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000)
-  const deadline = currentTimestampInSeconds + 86400
+ 
+  const impersonatedSigner = await ethers.getImpersonatedSigner(DAIHolder);
 
-  const uniswap = await ethers.getContractAt('IUniswapV2Router01', ROUTER)
+  const uniswap = await ethers.getContractAt('IUniswapV2Router01', ROUTER);
 
-  
-  const helpers = require("@nomicfoundation/hardhat-network-helpers");
-  await helpers.impersonateAccount(DAIHolder);
-  const impersonatedSigner = await ethers.getSigner(DAIHolder);
+  const uniswapFactory = await ethers.getContractAt("IUniswapV2Factory",FACTORY);
 
   const DaiContract = await ethers.getContractAt("Itokens", DAI);
 
   const UniContract = await ethers.getContractAt("Itokens", UNI);
+
+  await DaiContract.connect(impersonatedSigner).approve(
+    ROUTER,
+    ethers.parseEther("1000000")
+  );
+
+  await UniContract.connect(impersonatedSigner).approve(
+    ROUTER,
+    ethers.parseEther("1000000")
+  );
+
 
   const holderBalance = await DaiContract.balanceOf(DAIHolder);
   console.log(`Dai balance before ${holderBalance}`);
@@ -34,89 +48,75 @@ async function main() {
   const uniBalance = await UniContract.balanceOf(DAIHolder);
   console.log(`uniBalance ${uniBalance}`);
 
-  const amountA =  ethers.parseEther("2000");
-  const amountB =  ethers.parseEther("100");
+  const amountA =  ethers.parseEther("20");
+  const amountB =  ethers.parseEther("40");
+  const aMin = ethers.parseEther("0");
+  const bMin = ethers.parseEther("0");
+  const amountRADesired = ethers.parseEther("0");
+  const amountRBDesired = ethers.parseEther("0");
 
   //QUESTION 1: ADDING LIQUIDITY
-  await DaiContract.connect(impersonatedSigner).approve(ROUTER,amountB);
 
-  await UniContract.connect(impersonatedSigner).approve(ROUTER,amountA);
 
-  await uniswap.connect(impersonatedSigner).addLiquidity(DAI,UNI,amountA,amountA,0,0,impersonatedSigner.address,time);
+  const addLiquidity = await uniswap.connect(impersonatedSigner).addLiquidity(UNI,DAI,amountA,amountB,aMin,bMin,impersonatedSigner,deadline);
 
+  addLiquidity.wait()
+  
   const holderBalance1 = await DaiContract.balanceOf(DAIHolder);
   console.log(`Dai balance after ${holderBalance1}`);
 
   const uniBalance1 = await UniContract.balanceOf(DAIHolder);
-  console.log(`uniBalance after ${uniBalance1}`); */
+  console.log(`uniBalance after ${uniBalance1}`); 
 
-  // QUESTION 2: ADD LIQUIDITY ETH
+   //   uniswapFactory
 
+ const getLiquidityPair = await uniswapFactory.connect(impersonatedSigner).getPair(UNI, DAI);
+const pairContract = await ethers.getContractAt("Itokens", getLiquidityPair);
 
-//import { BigNumber } from "ethers";
-//import { providers } from "ethers";
+const pairLiquidity = await pairContract.balanceOf(impersonatedSigner);
 
+console.log(getLiquidityPair);
+console.log(pairContract);
+console.log(pairLiquidity);
 
- 
-  const ROUTER = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
-  const DAI = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
-  //const WETH = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
-  const UNI = "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984";
-  const WHALE = "0x748dE14197922c4Ae258c7939C7739f3ff1db573";
+const approvePairLiquidity = await pairContract
+ .connect(impersonatedSigner)
+ .approve(ROUTER, pairLiquidity);
 
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000)
-  const deadline = currentTimestampInSeconds + 86400
+const aprovalRecipt1 = await Promise.all([approvePairLiquidity.wait()]);
 
-  // Getting contract implementation
+console.log(aprovalRecipt1);
 
-  const Uniswap = await ethers.getContractAt("IUniswapV2Router01", ROUTER);
-  const DaiContract = await ethers.getContractAt("Itokens", DAI);
-  //const WETHContract = await ethers.getContractAt("IToken", WETH);
-  const UniContract = await ethers.getContractAt("Itokens", UNI);
-  
+console.log(approvePairLiquidity);
 
-  // Setting up impersonator
-  const helpers = require("@nomicfoundation/hardhat-network-helpers");
-  await helpers.impersonateAccount(WHALE);
-  const impersonatedSigner = await ethers.getSigner(WHALE);
+const txremoveLiqiudity = await uniswap
+ .connect(impersonatedSigner)
+ .removeLiquidity(
+  UNI,
+  DAI,
+  pairLiquidity,
+  amountRADesired,
+  amountRBDesired,
+  DAIHolder,
+  deadline
+ );
 
+await txremoveLiqiudity.wait();
+console.log(txremoveLiqiudity);
+console.log("Transaction hash:", txremoveLiqiudity.hash);
 
+console.log(
+ `USDC Balance after ${ethers.formatUnits(
+  await UniContract.balanceOf(DAIHolder),
+  6
+ )})`
+);
 
-
-  const txA = {
-      tokenA: DAI,
-      tokenB:UNI,
-      amountADesired: ethers.parseEther("40"),
-      amountBDesired: ethers.parseEther("60"),
-      amountAMin: ethers.parseEther("0"),
-      amountBMin: ethers.parseEther("0"),
-      to: impersonatedSigner.address,
-      deadline: deadline
-      
-  }
-
-  await DaiContract.connect(impersonatedSigner).approve(ROUTER, txA.amountADesired)
-  await UniContract.connect(impersonatedSigner).approve(ROUTER, txA.amountBDesired)
-  
-
-  console.log(`Adding DAI and UNI Liquidity...............`)
-  Uniswap.connect(impersonatedSigner).addLiquidity(
-    txA.tokenA,
-    txA.tokenB,
-    txA.amountADesired,
-    txA.amountBDesired,
-    txA.amountAMin,
-    txA.amountBMin,
-    txA.to,
-    txA.deadline
-  );
-
-    const holderBalanceAfter = await DaiContract.balanceOf(WHALE);
-    console.log(`Dai balance After ${holderBalanceAfter}`);
-
-    const uniBalanceAfter = await UniContract.balanceOf(WHALE);
-    console.log(`UNI Balance_After ${uniBalanceAfter}`);
-
+console.log(
+ `Dai Balance after ${ethers.formatEther(
+  await DaiContract.balanceOf(DAIHolder)
+ )})`
+);
 }
 
 main().catch((error) => {
